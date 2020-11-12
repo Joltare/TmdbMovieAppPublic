a2d92dcd8e61bf81068834bd28f877c6fce667a3
Richard Connolly <Richard.connolly@gmail.com>
2020-11-12 22:42:28 +0000

    E2E tests - Home page - Filtering by title - Exceptional case

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -1,6 +1,28 @@
-describe("Home Page", () => {
+let movies;    // List of movies from TMDB
+
+// Utility functions
+const filterByTitle = (movieList, string) =>
+  movieList.filter((m) => m.title.toLowerCase().search(string) !== -1);
+
+const filterByGenre = (movieList, genreId) =>
+  movieList.filter((m) => m.genre_ids.includes(genreId));
+
+
+  describe("Home Page ", () => {
+    before(() => {
+      // Get movies from TMDB and store in movies variable.
+      cy.request(
+        `https://api.themoviedb.org/3/discover/movie?api_key=${Cypress.env(
+          "TMDB_KEY"
+        )}&language=en-US&include_adult=false&include_video=false&page=1`
+      )
+        .its("body")    // Take the body of HTTP response from TMDB
+        .then((response) => {
+          movies = response.results
+        })
+    })
     beforeEach(() => {
-      cy.visit("/");
+      cy.visit("/")
     });
   
     describe("Base test", () => {
@@ -9,4 +31,17 @@ describe("Home Page", () => {
         cy.get(".badge").contains(20);
       });
     })
-  })
+    describe("Filtering", () => {
+        it("should only display movies with the specified title substring", () => {
+          let searchString = "m";
+          let matchingMovies = filterByTitle(movies, searchString);
+          cy.get("input").clear().type(searchString); // Enter m in text box
+          cy.get(".card").should("have.length", matchingMovies.length);
+          // Do a second test for certainty!
+          searchString = "o";
+          matchingMovies = filterByTitle(movies, searchString);
+          cy.get("input").clear().type(searchString)
+          cy.get(".card").should("have.length", matchingMovies.length);
+        });
+      });
+    });
