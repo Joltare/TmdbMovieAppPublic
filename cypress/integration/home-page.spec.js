ee06b58ac01bbcf9bdd78db80e38b33720fd1502
Richard Connolly <Richard.connolly@gmail.com>
2020-11-12 22:45:34 +0000

    E2E tests - Home page - Filtering by title and genre

--- cypress/integration/home-page.spec.js
+++ cypress/integration/home-page.spec.js
@@ -32,16 +32,43 @@ const filterByGenre = (movieList, genreId) =>
       });
     })
     describe("Filtering", () => {
-        it("should only display movies with the specified title substring", () => {
-          let searchString = "m";
-          let matchingMovies = filterByTitle(movies, searchString);
-          cy.get("input").clear().type(searchString); // Enter m in text box
-          cy.get(".card").should("have.length", matchingMovies.length);
-          // Do a second test for certainty!
-          searchString = "o";
-          matchingMovies = filterByTitle(movies, searchString);
-          cy.get("input").clear().type(searchString)
-          cy.get(".card").should("have.length", matchingMovies.length);
-        });
-      });
-    });
+        describe("By movie title" ,() => {
+          it("should display movies with 'p ' in the title", () => {
+            const searchString = 'p'
+            const matchingMovies = filterByTitle(movies, searchString );
+            cy.get("input").clear().type(searchString) ;
+            cy.get(".card").should("have.length", matchingMovies.length);
+            cy.get(".card").each(($card, index) => {
+              cy.wrap($card)
+              .find(".card-title")
+              .should("have.text", matchingMovies[index].title);
+            });
+          })
+          it("should display movies with 'o' in the title", () => {
+            const searchString = "o";
+            const matchingMovies = filterByTitle(movies, searchString);
+            cy.get("input").clear().type(searchString);
+            cy.get(".card").should("have.length", matchingMovies.length);
+            cy.get(".card").each(($card, index) => {
+              cy.wrap($card)
+              .find(".card-title")
+              .should("have.text", matchingMovies[index].title);
+            })
+          })
+        })
+        describe("By movie genre" ,() => {
+            it("should display movies with the specified genre only", () => {
+                const selectedGenreId = 35;
+                const selectedGenreText = "Comedy";
+                const matchingMovies = filterByGenre(movies, selectedGenreId);
+                cy.get("select").select(selectedGenreText); 
+                cy.get(".card").should("have.length", matchingMovies.length);
+                cy.get(".card").each(($card, index) => {
+                  cy.wrap($card)
+                    .find(".card-title")
+                    .should("have.text", matchingMovies[index].title);
+        })
+      })
+    })
+});
+});
